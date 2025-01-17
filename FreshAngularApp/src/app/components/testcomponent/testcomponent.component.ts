9eb44f4f973584a79f50994ebd4e1c8f73b58f49
anuraglearnproject@gmail.com <anuraglearnproject@gmail.com>
2025-01-17 21:30:37 +0530

    add files

--- FreshAngularApp/src/app/components/testcomponent/testcomponent.component.ts
+++ FreshAngularApp/src/app/components/testcomponent/testcomponent.component.ts
@@ -1,11 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-testcomponent',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './testcomponent.component.html',
   styleUrl: './testcomponent.component.css'
 })
 export class TestcomponentComponent {
-
+  clickStyle:any;
+  counter:number = 0;
+  clickMe():void {
+    this.clickStyle = {
+      "border": "1px solid black",
+      "fontWeight": "bold", 
+      "padding": "10px 100px"
+    }
+    ;
+    setTimeout(()=>{
+      this.clickStyle = {};      
+    }, 5000);
+    setInterval(()=>{
+      this.counter++;
+    }, 1000);
+  }
 }
