b6b7786711004186c9994f34c7839907d36fc738
anuraglearnproject@gmail.com <anuraglearnproject@gmail.com>
2025-01-17 20:34:20 +0530

    a

--- FreshAngularApp/src/app/app.component.ts
+++ FreshAngularApp/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TestcomponentComponent } from "./components/testcomponent/testcomponent.component";
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule, TestcomponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'FreshAngularApp';
+  title = 'Fresh Angular App';
+  showTestComponent:boolean = false;
 }
